7a113ec211f4d8daf4c788c208789b0b064a2830
Zhenhua <zhenhua1109@yahoo.com>
2022-08-29 12:21:46 -0700

    part 3 done

---
+++ part-3/App.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function App() {
+  return (
+    <div>
+      < Person name = "Rithm" age = "10" hobbies = {[ "coding", "lectures"]}/>
+      < Person name = "Zhen" age = "27" hobbies = {[ "balling", "movies"]}/>
+      < Person name = "Baldr" age = "8" hobbies = {[ "napping", "meowing"]}/>
+    </div>
+  );
+}
